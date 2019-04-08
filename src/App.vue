092cd1fd95bc9ac18153d847dd22f43b8d75480b
ekiriano <ekiriano34@gmail.com>
2019-04-08 13:37:23 +0200

    set up of visualisation and bibliography page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Visualisation</router-link> |
+      <router-link to="/bibliographie">Bibliographie</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Bibliographie.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bibliographie">
+    <h1>Bibliographie</h1>
+    <ul>
+      <li><a href="#">Wikipedia</a> </li>
+    </ul>
+  </div>
+</template>
