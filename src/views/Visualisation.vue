3c02ae938eb76409023b53822e0fb9546c7f3fc9
ekiriano <ekiriano34@gmail.com>
2019-04-09 21:34:07 +0200

    boyer moore end of  implementation ?

--- src/views/Visualisation.vue
+++ src/views/Visualisation.vue
@@ -26,18 +26,85 @@ export default {
   data() {
     return{
       motif : "",
-      texte : ""
+      texte : "",
+      tableChar: null,
+      tableDecalage : null,
     }
   },
   methods : {
-    pretraite(texte){
-
-    },
     visualize(){
 
     },
     nextStep(){
 
+    },
+    // boyer moore function
+    indexOf(){
+      var i, k;
+      var n = this.motif.length;
+      var m = this.texte.length;
+      if( n === 0 ) return n;
+      this.genTableChar();
+      this.genTableDecalage();
+
+      for(i = n -1 ; i <m;){
+        for(k=n-1;this.motif[k] == this.texte[k];--i,--k){
+          if(k === 0) return i;
+        }
+        i+= Math.max(this.tableDecalage[n-1-k],this.tableChar[this.texte[i]]);
+      }
+      return -1;
+    },
+    genTableChar(){
+      this.tableChar = new Uint32Array(256);
+      var n = this.texte.length;
+      var t = this.tableChar.length;
+      var i = 0 ;
+
+      for(;i<t;++i){
+        this.tableChar[i] = n ;
+      }
+      n--;
+      for(i=0;i<n;++i){
+        this.tableChar[this.motif[i]] = n-i;
+      }
+    },
+    genTableDecalage(){
+      var i , suffixe;
+      var n = this.motif.length;
+      var m = n-1;
+      var dernierPrefixe = n;
+      var table = new Uint32Array(n);
+
+      for(i = m ; i >=0; --i){
+        if(this.estPrefixe(this.motif,i+1)) dernierPrefixe = i+1;
+      }
+      table[m-i] = dernierPrefixe-i+m;
+      for(i = 0 ; i <n ;++i){
+        suffixe = this.longSuffixe(this.motif,i);
+        this.tableDecalage[suffixe] = m-i +suffixe;
+      }
+    },
+    estPrefixe(i){
+      var k = 0 ;
+      var n = this.motif.length;
+
+      for(; i<n ; ++i,++k){
+        if(this.motif[i] !== this.motif[k]){
+          return false;
+        }
+      }
+      return false;
+    },
+    longSuffixe(i){
+      var k = 0;
+      var n = this.motif.length;
+      var m = n - 1;
+
+      for(; i >= 0 && this.motif[i] == this.motif[m];--i,--m){
+        k+=1;
+      }
+      return k;
     },
     reset(){
       this.texte = "";
