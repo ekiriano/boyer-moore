bcdcd0acbbd0783553d095b06116a033b669aab2
ekiriano <ekiriano34@gmail.com>
2019-04-26 14:12:33 +0200

    Update Visualisation.Vue
    Implementation of BoyerMoore Algorithm working ==> Output is in console.log || TODO : Display of results on page , Display of step by step , Display of Complete Visualisation

--- src/views/Visualisation.vue
+++ src/views/Visualisation.vue
@@ -1,115 +1,124 @@
 <template>
   <div class="visualisation">
     <div>
-      <label> Motif :
-      <input type="text" placeholder="Motif" v-model="motif"></label>
-      <label> Texte :
-        <input type="text" placeholder="Texte" v-model="texte" ></label>
+      <label>
+        Motif :
+        <input type="text" placeholder="Motif" v-model="motif">
+      </label>
+      <label>
+        Texte :
+        <input type="text" placeholder="Texte" v-model="texte">
+      </label>
     </div>
     <div>
-      <button class="btn btn-primary mr-1" @click="visualize">Visualisation Complete <i class="fas fa-play"></i></button>
-      <button class="btn btn-success mr-1" @click="nextStep">Etape Par Etape <i class="fas fa-forward"></i></button>
-      <button class="btn btn-danger mr-1" @click="reset">Reset <i class="fas fa-undo"></i></button>
+      <button class="btn btn-primary mr-1" @click="main">
+        Visualisation Complete
+        <i class="fas fa-play"></i>
+      </button>
+      <button class="btn btn-success mr-1" @click="nextStep">
+        Etape Par Etape
+        <i class="fas fa-forward"></i>
+      </button>
+      <button class="btn btn-danger mr-1" @click="reset">
+        Reset
+        <i class="fas fa-undo"></i>
+      </button>
     </div>
     <h1>{{this.texte}}</h1>
   </div>
-
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'home',
-  components: {
-  },
+  name: "home",
+  components: {},
   data() {
-    return{
-      motif : "",
-      texte : "",
-      tableChar: null,
-      tableDecalage : null,
-    }
+    return {
+      motif: "",
+      texte: "",
+      index: -1,
+      localTab: [],
+      shiftTab: [],
+      borderTab: [],
+      tab: []
+    };
   },
-  methods : {
-    visualize(){
-
-    },
-    nextStep(){
-
-    },
-    // boyer moore function
-    indexOf(){
-      var i, k;
-      var n = this.motif.length;
-      var m = this.texte.length;
-      if( n === 0 ) return n;
-      this.genTableChar();
-      this.genTableDecalage();
-
-      for(i = n -1 ; i <m;){
-        for(k=n-1;this.motif[k] == this.texte[k];--i,--k){
-          if(k === 0) return i;
-        }
-        i+= Math.max(this.tableDecalage[n-1-k],this.tableChar[this.texte[i]]);
-      }
-      return -1;
-    },
-    genTableChar(){
-      this.tableChar = new Uint32Array(256);
-      var n = this.texte.length;
-      var t = this.tableChar.length;
-      var i = 0 ;
-
-      for(;i<t;++i){
-        this.tableChar[i] = n ;
-      }
-      n--;
-      for(i=0;i<n;++i){
-        this.tableChar[this.motif[i]] = n-i;
-      }
-    },
-    genTableDecalage(){
-      var i , suffixe;
-      var n = this.motif.length;
-      var m = n-1;
-      var dernierPrefixe = n;
-      var table = new Uint32Array(n);
-
-      for(i = m ; i >=0; --i){
-        if(this.estPrefixe(this.motif,i+1)) dernierPrefixe = i+1;
-      }
-      table[m-i] = dernierPrefixe-i+m;
-      for(i = 0 ; i <n ;++i){
-        suffixe = this.longSuffixe(this.motif,i);
-        this.tableDecalage[suffixe] = m-i +suffixe;
-      }
-    },
-    estPrefixe(i){
-      var k = 0 ;
-      var n = this.motif.length;
-
-      for(; i<n ; ++i,++k){
-        if(this.motif[i] !== this.motif[k]){
-          return false;
-        }
-      }
-      return false;
-    },
-    longSuffixe(i){
-      var k = 0;
-      var n = this.motif.length;
-      var m = n - 1;
-
-      for(; i >= 0 && this.motif[i] == this.motif[m];--i,--m){
-        k+=1;
-      }
-      return k;
-    },
-    reset(){
+  methods: {
+    visualize() {},
+    nextStep() {},
+    reset() {
       this.texte = "";
       this.motif = "";
+      this.index = -1;
+      this.localTab = [];
+      this.shiftTab = [];
+      this.borderTab = [];
+      this.tab = [];
+    },
+
+    matchCompletSuffixe() {
+      var n = this.motif.length;
+      var i = n;
+      var j = n + 1;
+      this.borderTab[i] = j;
+      while (i > 0) {
+        while (j <= n && this.motif[i - 1] !== this.motif[j - 1]) {
+          if (this.shiftTab[j] === 0) this.shiftTab[j] = j - i;
+
+          j = this.borderTab[j];
+        }
+        i--;
+        j--;
+        this.borderTab[i] = j;
+      }
+    },
+    matchSuffixePartiel() {
+      var n = this.motif.length;
+      var j;
+      j = this.borderTab[0];
+      for (var i = 0; i < n; i++) {
+        if (this.shiftTab[i] === 0) {
+          this.shiftTab[i] = j;
+        }
+        if (i === j) {
+          j = this.borderTab[j];
+        }
+      }
+    },
+    rechercheMotif() {
+      var l_motif = this.motif.length;
+      var l_texte = this.texte.length;
+      this.borderTab[l_motif + 1];
+      this.shiftTab[l_motif + 1];
+
+      for (var i = 0; i <= l_motif; i++) {
+        this.shiftTab[i] = 0;
+      }
+      this.matchCompletSuffixe();
+      this.matchSuffixePartiel();
+      var shift = 0;
+      while (shift <= l_texte - l_motif) {
+        var j = l_motif - 1;
+        while (j >= 0 && this.motif[j] === this.texte[shift + j]) {
+          j--;
+        }
+        if (j < 0) {
+          this.index++;
+          this.localTab[this.index] = shift;
+          shift += this.shiftTab[0];
+        } else {
+          shift += this.shiftTab[j + 1];
+        }
+      }
+    },
+    main() {
+      this.rechercheMotif();
+      for (var i = 0; i <= this.index; i++) {
+        console.log("Motif trouvé a la position " + this.localTab[i]);
+      }
     }
-  },
-}
+  }
+};
 </script>
